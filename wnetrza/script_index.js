2a36926e1118ec23f8d539952ff28a0ddd067b01
Mati Szulc <82444114+mati101811@users.noreply.github.com>
2025-11-19 21:45:55 +0100

    Refactor showDesign function and event listener

--- wnetrza/script_index.js
+++ wnetrza/script_index.js
@@ -1,6 +1,6 @@
 function showDesign(index) {
     // go to design<index>/index.html
-    window.location.href = `/project.html?id=${index}`;
+    window.location.href = `project.html?id=${index}`;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -41,4 +41,5 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         })
         .catch(err => console.error("Błąd wczytywania danych:", err));
+
 });
