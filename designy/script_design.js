60f556a4f2d4bda3aebc3cc74b86703ec1b289e7
Mati Szulc <82444114+mati101811@users.noreply.github.com>
2025-11-19 14:53:55 +0100

    Refactor script_design.js for improved readability

--- designy/script_design.js
+++ designy/script_design.js
@@ -285,8 +285,9 @@ function setActiveModalVariant(code) {
 
 
 function goToGallery() {
-    window.location.href = `designy/gallery.html?project_id=${projectId}&design_id=${designId}`;
+    window.location.href = `gallery.html?project_id=${projectId}&design_id=${designId}`;
 
 }
+
 
 
